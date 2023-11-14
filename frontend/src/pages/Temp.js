648ddbb8235fa51ef7e7a3ad1f227dcf78116b4a
Thien <pth.a15.33.tran.thien@gmail.com>
2023-11-14 12:13:49 +0900

    be,test

--- frontend/src/pages/Temp.js
+++ frontend/src/pages/Temp.js
@@ -1,9 +1,16 @@
 import { useState } from 'react';
 
 const Temp = () => {
+    const [temp, setTemt] = useState();
+    const es = new EventSource("http://127.0.0.1:8000/api/event-stream/")
+    es.onmessage = (e) => {
+        setTemt(e.data)
+    };
     return (
         <div>
             MAP
+            <br/>
+            {temp}
         </div>
     )
 }
