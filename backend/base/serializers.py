7d172914534f1fd4f4738189f9f9ebf99bd2fa63
Thien <pth.a15.33.tran.thien@gmail.com>
2024-01-11 14:12:32 +0900

    delete

--- backend/base/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Temp
-
-class TempSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Temp
-        fields = "__all__"
